bea77c36f90656eb5e88dce30572fe5471ebdcf7
Parikshit Sharma <parikshit.p450@gmail.com>
2024-10-05 08:10:13 +0530

    added hero image loader

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,12 +1,28 @@
+import { useState, useEffect } from 'react';
 import hero_img from '/src/assets/hero.jpg';
+
 const Hero = () => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = hero_img;
+    img.onload = () => setImageLoaded(true);
+  }, []);
+
   return (
     <section>
-        <div className='w-full object-cover bg-black'>
-           <img src={hero_img} className="w-full opacity-80 " alt="" />
-        </div>
+      <div className='w-full h-screen relative'>
+        <div className='w-full h-full bg-green-950 absolute top-0 left-0'></div>
+        <img 
+          src={hero_img} 
+          className={`w-full h-full object-cover absolute top-0 left-0 transition-opacity duration-1000 ${imageLoaded ? 'opacity-80' : 'opacity-0'}`} 
+          alt="Hero" 
+          onLoad={() => setImageLoaded(true)}
+        />
+      </div>
     </section>
   )
 }
 
-export default Hero;
+export default Hero
