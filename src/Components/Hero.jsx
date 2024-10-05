1a7ddee9d4cce5f8c216deb4dcdfef73ab3bd415
Parikshit Sharma <parikshit.p450@gmail.com>
2024-10-05 08:04:13 +0530

    fixed hero glitch

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,30 +1,10 @@
-import { useState, useEffect } from 'react';
 import hero_img from '/src/assets/hero.jpg';
-
 const Hero = () => {
-  const [imageLoaded, setImageLoaded] = useState(false);
-
-  useEffect(() => {
-    const img = new Image();
-    img.src = hero_img;
-    img.onload = () => setImageLoaded(true);
-  }, []);
-
   return (
     <section>
-      <div className='w-full h-screen relative bg-green-950'>
-        <div 
-          className={`absolute inset-0 transition-opacity duration-1000 ${
-            imageLoaded ? 'opacity-100' : 'opacity-0'
-          }`}
-        >
-          <img 
-            src={hero_img} 
-            className="w-full h-full object-cover opacity-80" 
-            alt="Hero" 
-          />
+        <div className='w-full object-cover bg-black'>
+           <img src={hero_img} className="w-full opacity-80 " alt="" />
         </div>
-      </div>
     </section>
   )
 }
